cbce0b0cfc1b3a2e8e0516ec76702d853782c441
fmtacuri <56895072+fmtacuri@users.noreply.github.com>
2022-10-18 22:26:23 -0500

    Add files via upload

---
+++ src/main/java/com/grupo02/SceneryOne/SceneryOneApplication.java
@@ -0,0 +1,13 @@
+package com.grupo02.SceneryOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SceneryOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SceneryOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo02/SceneryOne/SceneryOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo02.SceneryOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SceneryOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
